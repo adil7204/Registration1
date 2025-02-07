3f0ca029e42703813444d180a774a04228515735
adil <adil@gmail.com>
2025-02-07 23:10:30 +0530

    Adil

---
+++ main/java/com/prog/SbtRegistrationApp3Application.java
@@ -0,0 +1,13 @@
+package com.prog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbtRegistrationApp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbtRegistrationApp3Application.class, args);
+	}
+
+}
